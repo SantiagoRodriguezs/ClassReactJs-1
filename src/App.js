592e189ea73c530e5dc34a1e728caed7bcc50cfc
Santi <ssanchezrodriguez931@gmail.com>
2024-10-28 08:29:28 -0500

    Agregar Front similar a una red social

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <h1>Red Social</h1>
       </header>
+      <div className="publicacion">
+        <h2>Camilo Sanchez</h2>
+        <p>Disfrutando de un hermoso día en la playa!</p>
+        <div className="interacciones-publicacion">
+          <p>👍 23 likes</p>
+          <p>💬 3 comentarios</p>
+        </div>
+        <div className="comentarios">
+          <p className="comentario">Increíble!</p>
+          <p className="comentario">Que envidia!</p>
+          <p className="comentario">Disfruta mucho!</p>
+        </div>
+      </div>
+
+      <div className="publicacion">
+        <h2>Mariana Pajon</h2>
+        <p>Mi primera carrera de 10k! 💪</p>
+        <div className="interacciones-publicacion">
+          <p>👍 45 likes</p>
+          <p>💬 3 comentarios</p>
+        </div>
+        <div className="comentarios">
+          <p className="comentario">Felicidades!</p>
+          <p className="comentario">Eres una campeona!</p>
+          <p className="comentario">Increíble esfuerzo!</p>
+        </div>
+      </div>
+
+      <div className="AgPublicacion">
+        <span>Agregar publicacion</span>
+      </div>
     </div>
   );
 }
